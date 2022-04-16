d282cd049a32829eba9b3951f192820b00854ff3
Kadir Nar <kadir.nar@hotmail.com>
2022-04-17 01:58:58 +0300

    Add files via upload

---
+++ model_test.py
@@ -0,0 +1,26 @@
+from pyexpat import model
+import unittest
+from model_download import Yolov5TestConstants, download_yolov5n_model
+MODEL_DEVICE = "cpu"
+CONFIDENCE_THRESHOLD = 0.3
+IMAGE_SIZE = 320
+
+
+class TestYolov5Model(unittest.TestCase):
+    def test_load_model(self):
+        from model import Yolov5DetModel
+
+        download_yolov5n_model()
+
+        yolov5_detection_model = Yolov5DetModel(
+            model_path=Yolov5TestConstants.YOLOV5N_MODEL_PATH,
+            confidence_threshold=CONFIDENCE_THRESHOLD,
+            device=MODEL_DEVICE,
+            load_at_init= True,
+        )
+
+        self.assertNotEqual(yolov5_detection_model.model, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
