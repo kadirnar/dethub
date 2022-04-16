4a8b314ea79d198baf4a4f6ca230b8bf039a56b1
Kadir Nar <kadir.nar@hotmail.com>
2022-04-17 01:47:53 +0300

    upgrade model.py and added model_test and download

--- model.py
+++ model.py
@@ -1,72 +1,46 @@
-class DetectionModel():
+from typing import Optional
+import numpy as np
+
+
+class DetectionModel:
     def __init__(
         self,
         model_path: str,
-        device: str = "cpu",
-        confidence_threshold: float = 0.5,
-        image_size: int = 512,
+        device: Optional[str] = None,
+        confidence_threshold: float = 0.3,
+        load_at_init: bool = True,
+        image_size: int = None,
     ):
+
         self.model_path = model_path
+        self.model = None
         self.device = device
         self.confidence_threshold = confidence_threshold
         self.image_size = image_size
-        self.model = None
+
+        # automatically load model if load_at_init is True
+        if load_at_init:
+            self.load_model()
 
     def load_model(self):
         raise NotImplementedError()
 
-    def model_predict(self, image, image_size):
-        raise NotImplementedError()
 
-    def object_detection_list(self, image):
-        raise NotImplementedError()
-
-
-class Yolov5Model(DetectionModel):
+class Yolov5DetModel(DetectionModel):
     def load_model(self):
-
+        """
+        Detection model is initialized and set to self.model.
+        """
         try:
             import yolov5
         except ImportError:
             raise ImportError(
-                "Please install yolov5 with `pip install yolov5`"
-            )
+                'Please run "pip install -U yolov5" ' "to install YOLOv5 first for YOLOv5 inference.")
 
+        # set model
         try:
-
-            model = yolov5.load(self.model_path, self.device)
+            model = yolov5.load(self.model_path, device=self.device)
             model.conf = self.confidence_threshold
             self.model = model
         except Exception as e:
-            raise TypeError("Model path is not a valid yolov5 model:", e)
-
-    def model_predict(self, image, image_size):
-        if self.model is None:
-            raise TypeError("Model is not loaded")
-
-        prediction = self.model.predict(image, self.image_size)
-        self.prediction = prediction
-
-    def object_predcition_list(self, image):
-        if self.prediction is None:
-            raise TypeError("Model prediction is not loaded")
-
-        object_predic_list = []
-
-        for _, xyxy in enumerate(self.prediction):
-            for pred in xyxy.cpu.detach().numpy():
-                x1, y1, x2, y2 = int(pred[0]), int(
-                    pred[1]), int(pred[2]), int(pred[3])
-                bbox = [x1, y1, x2, y2]
-                score = pred[4]
-                category_id = int(pred[5])
-                category_name = self.model.classes[category_id]
-                object_predic_list.append(
-                    {
-                        "bbox": bbox,
-                        "score": score,
-                        "category_id": category_id,
-                        "category_name": category_name,
-                    }
-                )
-        self.pred_list = object_predic_list
+            raise TypeError("model_path is not a valid yolov5 model path: ", e)
