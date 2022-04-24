8cdf9a504ddb2ec2804ee6c1762548b024af6591
Kadir Nar <kadir.nar@hotmail.com>
2022-04-24 21:48:53 +0300

    mimari yapısı basitleştirildi

--- predict.py
+++ predict.py
@@ -1,16 +1,9 @@
-from matplotlib import image
-from dethub.utils.data_utils import  download_yolov5n_model
-from dethub.yolov5_model import Yolov5DetectionModel
+from dethub.utils.vis import yolov5_visualization
+from dethub.utils.yolov5_predict import model_prediction
 import cv2
 
-download_yolov5n_model()
-confidence_threshold = 0.5
-device = "cpu"
-model_path = "dethub/yolov5/yolov5n.pt"
-img_path = "dethub/data/small-vehicles1.jpeg"
-img = cv2.imread(img_path)
 
+img = cv2.imread("dethub/data/small-vehicles1.jpeg") # img path
+prediction_list =  model_prediction(img, 0.5, "cpu", "dethub/yolov5/yolov5n.pt") # model prediction
+img = yolov5_visualization(img, prediction_list)     # visualization
 
-detection_model = Yolov5DetectionModel(model_path, confidence_threshold, device)
-detection_model.load_model()
-prediction_list = detection_model.object_prediction_list(img)
