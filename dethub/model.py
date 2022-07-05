cb24e47c6ce4d2e3539547d298ae86d1e256d029
kadirnar <kadir.nar@hotmail.com>
2022-07-05 17:44:41 +0300

    update

--- dethub/model.py
+++ dethub/model.py
@@ -1,8 +1,5 @@
 from typing import Optional
 
-from dethub.utils.data_utils import imshow
-
-
 class DetectionModel:
     def __init__(
         self,
@@ -59,7 +56,7 @@ class Yolov5(DetectionModel):
 
     def object_prediction_list(self, img):
         prediction = self.model(img)
-        prediction_list = {}
+        prediction_list = []
 
         for _, image_predictions_in_xyxy_format in enumerate(prediction.xyxy):
             for pred in image_predictions_in_xyxy_format.cpu().detach().numpy():
@@ -67,16 +64,14 @@ class Yolov5(DetectionModel):
                 bbox = [x1, y1, x2, y2]
                 score = pred[4]
                 category_name = self.model.names[int(pred[5])]
-                category_id = int(pred[5])
-                prediction_list = {
-                    "bbox": bbox,
-                    "score": score,
-                    "category_name": category_name,
-                    "category_id": category_id,
-                }
+                category_id = pred[5]
+                prediction_list.append(
+                    {"bbox": bbox, "score": score, "category_name": category_name, "category_id": category_id}
+                )
+
         self.prediction_list = prediction_list
 
-
+        
 class Torchvision(DetectionModel):
     def load_model(self):
         import torch
@@ -106,36 +101,7 @@ class Torchvision(DetectionModel):
             prediction_list.append([prediction_boxes[i], prediction_class[i], prediction_score[i]])
         self.prediction_list = prediction_list
 
-        return prediction_list
-
-    def visualization(self, img):
-        import cv2
-        import numpy as np
-
-        from dethub.utils.visualize import _COLORS
-
-        pred_boxes = [i[0] for i in self.prediction_list]
-
-        for i, box in enumerate(pred_boxes):
-            score = self.prediction_list[i][2]
-            color = (_COLORS[i % len(_COLORS)][::-1] * 255).astype(np.uint8)
-            color = (int(color[0]), int(color[1]), int(color[2]))
-            text = "{}:{:.1f}%".format(self.prediction_list[i][1], score * 100)
-            txt_color = (0, 0, 0) if np.mean(_COLORS[i % len(_COLORS)]) > 0.5 else (255, 255, 255)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
-            x, y, w, h = int(box[0][0]), int(box[0][1]), int(box[1][0] - box[0][0]), int(box[1][1] - box[0][1])
-            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-            txt_bk_color = (_COLORS[i % len(_COLORS)][::-1] * 255).astype(np.uint8)
-            txt_bk_color = (int(txt_bk_color[0]), int(txt_bk_color[1]), int(txt_bk_color[2]))
-            cv2.rectangle(img, (x, y), (x + txt_size[0] + 3, y + txt_size[1] + 3), txt_bk_color, -1)
-            cv2.putText(img, text, (x + 1, y + txt_size[1] + 1), font, 0.4, txt_color, 1, cv2.LINE_AA)
-
-        cv2.imshow("img", img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-
+        
 class TensorflowHub(DetectionModel):
     def load_model(self):
         import tensorflow as tf
@@ -154,32 +120,25 @@ class TensorflowHub(DetectionModel):
 
         category_mapping = {str(i): COCO_CLASSES[i] for i in range(len(COCO_CLASSES))}
         img = to_float_tensor(image)
+        npy_img = cv2.imread(image)
         prediction_result = self.model(img)
 
-        image_height, image_width = img.shape[0], img.shape[1]
+        self.image_height, self.image_width = img.shape[0], img.shape[1]
         boxes = prediction_result["detection_boxes"][0].numpy()
         scores = prediction_result["detection_scores"][0].numpy()
         category_ids = prediction_result["detection_classes"][0].numpy()
-
-        prediction_list = {}
         with tf.device(self.device):
             for i in range(min(boxes.shape[0], 100)):
                 if scores[i] >= self.confidence_threshold:
                     score = float(scores[i])
                     category_id = int(category_ids[i])
+
                     category_names = category_mapping[str(category_id - 1)]
                     box = [float(box) for box in boxes[i]]
                     x1, y1, x2, y2 = (
-                        int(box[1] * image_width),
-                        int(box[0] * image_height),
-                        int(box[3] * image_width),
-                        int(box[2] * image_height),
+                        int(box[1] * self.image_width),
+                        int(box[0] * self.image_height),
+                        int(box[3] * self.image_width),
+                        int(box[2] * self.image_height),
                     )
                     bbox = [x1, y1, x2, y2]
-                    prediction_list = {
-                        "bbox": bbox,
-                        "score": score,
-                        "category_id": category_id,
-                        "category_name": category_names,
-                    }
-        self.prediction_list.append(prediction_list)
