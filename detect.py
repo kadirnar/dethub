3c133cc2c07edd463acb11a3bbf97e959003fd86
Kadir Nar <kadir.nar@hotmail.com>
2022-07-07 16:53:06 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -11,6 +11,19 @@ def get_prediction(image, detection_model):
     vis(image, object_prediction_list)
 
 
-img = "data/highway1.jpg"
-detection_model = Yolov5(model_path="dethub/models/yolov5/yolov5n.pt", device="cpu", confidence_threshold=0.5)
-get_prediction(img, detection_model)
+def run(model_type, model_path, image_path, device="cpu", confidence_threshold=0.5):
+    if model_type == "yolov5":
+        detection_model = Yolov5(model_path, device, confidence_threshold)
+
+    elif model_type == "torchvision":
+        detection_model = TorchVision(model_path, device, confidence_threshold)
+
+    elif model_type == "tensorflow":
+        detection_model = TensorflowHub(model_path, device, confidence_threshold)
+
+    get_prediction(image_path, detection_model)
+
+
+# run("torchvision", "dethub/models/torchvision/fasterrcnn_resnet50_fpn.pth", "data/highway1.jpg")
+# run('yolov5', 'dethub/models/yolov5/yolov5n.pt', 'data/highway1.jpg')
+# run('tensorflow', 'https://tfhub.dev/tensorflow/efficientdet/d0/1', 'data/highway1.jpg')
