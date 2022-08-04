c95f64f3f74ddba8fadcfb58f7887cb420a17123
kadirnar <kadir.nar@hotmail.com>
2022-08-04 22:55:17 +0100

    added model.yaml

--- dethub/utils/file_utils.py
+++ dethub/utils/file_utils.py
@@ -2,17 +2,17 @@ import urllib.request
 from os import path
 from pathlib import Path
 from typing import Optional
+import yaml
 
-
-class ModelDownload:
-    def __init__(
-        self, url: Optional[str] = None, model_path: Optional[str] = None
-    ):
-        self.url = url
-        self.model_path = model_path
+class FileUtils:
+    def __init__(self, yaml_path: str, model_name: str, destination_path: str):
+        self.yaml_path = yaml_path
+        self.model_name = model_name
+        self.destination_path = destination_path
+        
 
     @staticmethod
-    def download_from_url(self, from_url: str, to_path: str):
+    def download_from_url(from_url: str, to_path: str):
 
         Path(to_path).parent.mkdir(parents=True, exist_ok=True)
 
@@ -21,41 +21,19 @@ class ModelDownload:
                 from_url,
                 to_path,
             )
-
-    @staticmethod
-    def yolov5n(self, destination_path: Optional[str] = None):
-        YOLOV5N_MODEL_URL = "https://github.com/ultralytics/yolov5/releases/download/v6.0/yolov5n.pt"
-        YOLOV5N_MODEL_PATH = "dethub/models/yolov5/yolov5n.pt"
-
-        if destination_path is None:
-            destination_path = YOLOV5N_MODEL_PATH
-
-        Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
-
-        if not path.exists(destination_path):
+    
+    def model_download(self):
+        import yaml
+        with open(self.yaml_path, "r") as stream:
+            self.model_name = yaml.safe_load(stream)["model_name"]
+        
+        if self.destination_path is None:
+            self.destination_path = self.model_name[1]
+        
+        Path(self.destination_path).parent.mkdir(parents=True, exist_ok=True)
+        if not path.exists(self.destination_path):
             urllib.request.urlretrieve(
-                YOLOV5N_MODEL_URL,
-                destination_path,
-            )
-
-        return destination_path
-
-    @staticmethod
-    def torchvision(self, destination_path: Optional[str] = None):
-        TORCHVISION_MODEL_URL = "https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth"
-        TORCHVISION_MODEL_PATH = (
-            "dethub/models/torchvision/fasterrcnn_resnet50_fpn.pth"
-        )
-
-        if destination_path is None:
-            destination_path = TORCHVISION_MODEL_PATH
-
-        Path(destination_path).parent.mkdir(parents=True, exist_ok=True)
-
-        if not path.exists(destination_path):
-            urllib.request.urlretrieve(
-                TORCHVISION_MODEL_URL,
-                destination_path,
-            )
-
-        return destination_path
+                self.model_name[0],
+                self.destination_path,
+            )       
+            
